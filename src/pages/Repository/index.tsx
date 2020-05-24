235c2c38f7a34edf0e13d8c8b450fc6da92327bc
Pedro Wanderley <peu-zico>
2020-05-24 11:35:50 -0300

    repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouteMatch, Link } from "react-router-dom";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -10,8 +11,57 @@ interface RepositoryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    // desta primeira forma, chamando duas vezes à api com a sintaxe
+    // de .then(), as duas chamadas são feitas ao mesmo tempo
+    // se fizer duas chamadas à api na sintaxe de await, uma requisição
+    // só será feita após a primeira terminar. Isso irá demorar mais
+    api.get(`/repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`/repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+
+    // ou então faz na sintaxe de await Promise.all() para que as duas
+    //requisições sejam chamadas ao mesmo tempo e usando a sintaxe de await
+    // aqui a segunda requisição não depende da primeira terminar para ser executada
+
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+    // }
+  }, [params.repository]);
 
   return (
     <>
@@ -23,42 +73,48 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img
-            src="https://avatars3.githubusercontent.com/u/51208665?v=4"
-            alt="Pedrohw12"
-          />
-          <div>
-            <strong>pedrohw12/explace-backend</strong>
-            <p> descrição do repositório </p>
-          </div>
-        </header>
-        <ul>
-          <li>
-            <strong> 1880 </strong>
-            <span>starts</span>
-          </li>
-          <li>
-            <strong> 1880 </strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong> 1880 </strong>
-            <span>Issues</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+      {repository ? (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong> {repository.full_name} </strong>
+              <p> {repository.description} </p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong> {repository.stargazers_count} </strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong> {repository.forks_count} </strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong> {repository.open_issues_count} </strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      ) : (
+        <p>CARREGANDO</p>
+      )}
 
       <Issues>
-        <Link to={"lkj"}>
-          <div>
-            <strong>sdasas</strong>
-            <p>asdsada</p>
-          </div>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </Link>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   );
